303c699c00225d016f1645d4667071d889a7b9e2
Sandwichzzy <62085707+Sandwichzzy@users.noreply.github.com>
2025-09-15 02:35:54 +0000

    constants

---
+++ constants.go
@@ -0,0 +1,15 @@
+package main
+
+const pi=3.14
+const GRAVITY=9.8
+
+func main(){
+	const days int=7;
+	const (
+		monday=1
+		tuesday=2
+		wenesday=3
+		thursday int =4
+	) //group constants immutable
+	// name:="john" XX not constants
+}
