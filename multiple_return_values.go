6604080a29ae6acab867189b43c92329637d39ef
Sandwichzzy <62085707+Sandwichzzy@users.noreply.github.com>
2025-09-15 12:58:34 +0000

    multiple_return

---
+++ multiple_return_values.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+
+func main() {
+		// func functionName(parameter1 type1, parameter2 type2,...) (returnType1, returnType2,...){
+		// code block
+		// return returvalue1, returnValue2,...
+		// }
+		q,r:=divide(5,2)
+		fmt.Printf("quotient:%v remainder:%v\n",q,r)
+
+		result,err:=compare(5,5)
+		if err!=nil{
+			fmt.Println("Error",err)
+		}else{
+			fmt.Println(result)
+		}
+}
+
+func divide(a,b int) (quotient int,remainder int){
+	quotient =a/b
+	remainder =a%b
+	return quotient,remainder
+}
+
+func compare(a,b int) (string,error){
+	if(a>b){
+		return "a is greater than b",nil
+	}else if (b>a){
+		return "b is greater than a",nil
+	}else{
+		return "",errors.New("Unable to compare which is greater")
+	}
+}
