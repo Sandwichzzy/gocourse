e0a7d8c62544d95bc6733fdfd049aa8b09a25be4
Sandwichzzy <62085707+Sandwichzzy@users.noreply.github.com>
2025-09-16 04:08:46 +0000

    exit

---
+++ exit.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+//- Practical Use Cases
+// Error Handling
+// Termination Conditions
+// Exit Codes
+// -Best Practices
+// Avoid Deferred Actions
+// Status Codes
+// Avoid Abusive Use
+func main() {
+
+	defer fmt.Println("Deferred statement")
+
+	fmt.Println("Starting the main function")
+
+	// Exit with status code of 1
+	os.Exit(1)
+
+	// This will never be executed
+	fmt.Println("End of main function")
+
+}
