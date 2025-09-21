3553537846dd551823689e47e62903dc0d8727e0
Sandwichzzy <62085707+Sandwichzzy@users.noreply.github.com>
2025-09-21 05:42:35 +0000

    use string function

--- intermediate/strings_runes.go
+++ intermediate/strings_runes.go
@@ -1,5 +1,10 @@
 package intermediate
 
+//rune是int32的别名，它能够表示一个Unicode字符的码点。
+//Unicode码点可以表示世界上几乎所有书写系统中的字符，包括ASCII字符（因为ASCII是Unicode的子集）。
+//ASCII字符集只包含128个字符（0-127），而Unicode字符集则包含超过100万个字符，涵盖了多种语言和符号。
+//rune则允许我们按照Unicode字符来处理字符串，每个rune对应一个Unicode字符，无论这个字符由多少个字节表示。
+
 import (
 	"fmt"
 	"unicode/utf8"
@@ -19,7 +24,7 @@ import (
 // Efficiency
 func main() {
 	message:="Hello,\nGo!"
-	message2:="Hello,\tGo!"
+	message2:="Hello,\tGo!" //Hello,  Go!
 	message3:="Hello,\rGo!" //Go!lo
 	rawMessage:=`Hello\nGo`
 
@@ -28,30 +33,42 @@ func main() {
 	fmt.Println(message3)
 	fmt.Println(rawMessage)
 
-	fmt.Println("Length of rawmessage variable is", len(rawMessage))
+	fmt.Println("Length of rawmessage variable is", len(rawMessage)) //Length of rawmessage variable is 9
 
-	fmt.Println("The first character in message var is", message[0]) // ASCII
+	fmt.Println("The first character in message var is", message[0]) // ASCII is 72
 
 	greeting:="hello "
 	name:="Alice"
-	fmt.Println(greeting+name)
+	fmt.Println(greeting+name) //hello Alice
 
 	str1 := "Apple"  // A has an ASCII value of 65
 	str := "apple"   // a has an ASCII value of 97
 	str2 := "banana" // b has an ASCII value of 98
 	str3 := "app"    // a has an ASCII value of 97
-	fmt.Println(str1 < str2)
-	fmt.Println(str3 < str1)
-	fmt.Println(str > str1)
-	fmt.Println(str > str3)
+	fmt.Println(str1 < str2) //true
+	fmt.Println(str3 < str1) //false
+	fmt.Println(str > str1) //true
+	fmt.Println(str > str3) //true
 
+
+// 	Character at index 0 is H
+// Character at index 1 is e
+// Character at index 2 is l
+// Character at index 3 is l
+// Character at index 4 is o
+// Character at index 5 is ,
+// Character at index 6 is 
+
+// Character at index 7 is G
+// Character at index 8 is o
+// Character at index 9 is !
 	for i, char := range message {
-		 fmt.Printf("Character at index %d is %c\n", i, char)
+		 fmt.Printf("Character at index %d is %c\n", i, char) 
 		// fmt.Printf("%v\n", char)
 		//fmt.Printf("%x\n", char)
 	}
 
-	fmt.Println("RuneCount",utf8.RuneCountInString(greeting))
+	fmt.Println("RuneCount",utf8.RuneCountInString(greeting)) //RuneCount 6
 	// fmt.Println("Length",len(greeting))
 
 	//string immutable
@@ -61,19 +78,19 @@ func main() {
 	//Rune is alias for int32 represents(Unicode value) integer value
 	var ch rune ='a'
 	jch :='日'
-	fmt.Println(ch)
-	fmt.Println(jch)
+	fmt.Println(ch) //97
+	fmt.Println(jch) //26085
 
-	fmt.Printf("%c\n", ch)
-	fmt.Printf("%c\n", jch)
+	fmt.Printf("%c\n", ch) //a
+	fmt.Printf("%c\n", jch) //日
 
 	cstr := string(ch)
-	fmt.Println(cstr)
+	fmt.Println(cstr) //a
 
-	fmt.Printf("Type of cstr is %T\n", cstr)
+	fmt.Printf("Type of cstr is %T\n", cstr) //Type of cstr is string
 
 	const NIHONGO = "日本語" // Japanese text
-	fmt.Println(NIHONGO)
+	fmt.Println(NIHONGO) //日本語
 
 	jhello := "こんにちは" // Japanese "Hello"
 	for _, runeValue := range jhello {
@@ -81,6 +98,6 @@ func main() {
 	}
 
 	r := '😊'
-	fmt.Printf("%v\n", r)
-	fmt.Printf("%c\n", r)
+	fmt.Printf("%v\n", r) //128522
+	fmt.Printf("%c\n", r) //😊
 }
---
+++ string_function.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
+
+func main() {
+	str:= "Hello Go!"
+
+	fmt.Println(len(str))
+
+	str1:="Hello"
+	str2:="World"
+
+	result:=str1 +" "+str2
+	fmt.Println(result) // Hello World
+
+	fmt.Println(str[0]) //72 ASCII
+	var r rune ='H' 
+	fmt.Println(r) //72
+	fmt.Println(str[1:5]) //ello [)
+
+	// s := "Hello, 世界"
+  // for _, r := range s {
+  //     fmt.Printf("字符: %c, Unicode码点: %U, unicode:%v\n", r, r,r)
+  // }
+
+	//string conversion
+	num:=18
+	str3:=strconv.Itoa(num) //int to string
+	fmt.Println(len(str3)) //2
+	
+	// strings splitting
+	fruits := "apple, orange, banana"
+	fruits1 := "apple-orange-banana"
+	parts := strings.Split(fruits, ",")
+	parts1 := strings.Split(fruits1, "-")
+	fmt.Println(parts) //[apple orange banana]
+	fmt.Println(parts1) // [apple orange banana]
+
+	//strings join
+	countries:=[]string{"Germany","France","Italy"}
+	joined:=strings.Join(countries,",")
+	fmt.Println(joined) //Germany,France,Italy
+
+	//strings.Contains
+	fmt.Println(strings.Contains(str,"Go")) //true
+
+	//strings.Replace(s string ，old string，new string, n int 替换前几个)
+	replaced:=strings.Replace(str,"Go","World",1) //Hello World!
+	fmt.Println(replaced)
+
+	//strings.TrimSpace
+	strwspace := " Hello Everyone! "
+	fmt.Println(strwspace)
+	fmt.Println(strings.TrimSpace(strwspace)) //移除空格
+
+	// //strings.ToLower strings.ToUpper'
+	// fmt.Println(strings.ToLower(strwspace))
+	// fmt.Println(strings.ToUpper(strwspace))
+
+	//strings.Repeat
+	fmt.Println(strings.Repeat("foo",3)) //foofoofoo
+
+	//strings.Count
+	fmt.Println(strings.Count("Hello", "l")) //2
+	fmt.Println(strings.HasPrefix("Hello", "he")) //false
+	fmt.Println(strings.HasSuffix("Hello", "lo")) //true
+	fmt.Println(strings.HasSuffix("Hello", "la"))
+
+	//regexp
+	str5 :="Hello,123 Go 11!"
+	re:=regexp.MustCompile(`\d+`) //\d+代表一个和多个数字应该一起出现，不包括空格
+	//-1 代表寻找该正则表达式的所有匹配项
+	matches:=re.FindAllString(str5,-1)
+	fmt.Println(matches) //[123 11]
+
+	//unicode/utf8
+	str6 := "Hello, 世界"
+	fmt.Println(utf8.RuneCountInString(str6)) //9
+
+	//strings.Builder
+	var builder strings.Builder
+
+	//write some strings
+	builder.WriteString("Hello")
+	builder.WriteString(", ")
+	builder.WriteString("world!")
+
+	//Convert builder to a string
+	resultString := builder.String()
+	fmt.Println(resultString) //Hello, world!
+
+	//using writerune to add a character
+	builder.WriteRune(' ')
+	builder.WriteString("How are you")
+
+	resultString = builder.String()
+
+	fmt.Println(resultString) //Hello, world! How are you
+
+	//Reset the builder
+	builder.Reset()
+	builder.WriteString("Starting fresh!")
+	resultString=builder.String()
+	fmt.Println(resultString) //Starting fresh!
+}
