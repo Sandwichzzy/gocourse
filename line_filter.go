3f36b8eab16a38dfee72a7bc3926ed1fc0398f5f
Sandwichzzy <62085707+Sandwichzzy@users.noreply.github.com>
2025-10-06 06:26:56 +0000

    line filter/bufio scanner

---
+++ line_filter.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// 1. Key Components
+// Reading Lines Individually
+// Applying Criteria or Transformations
+// Processing Each Line
+// Filtering Lines Based on Content
+// Removing Empty Lines
+// Transforming Line Content
+// Filtering Lines by Length
+// *bufio` Package
+// -Scanner.Scan()
+// -Scanner.Text()
+func main() {
+
+	file,err:=os.Open("examples.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	defer file.Close()
+	scanner:=bufio.NewScanner(file)
+
+	lineNumber:=1
+
+	// keyword to filter lines
+	keyword:="important"
+	//Read and filter lines
+	for scanner.Scan() {
+		line :=scanner.Text()
+		if strings.Contains(line, keyword) {
+			updatedLine := strings.ReplaceAll(line, keyword, "necessary")
+			fmt.Printf("%d Filtered line: %v\n", lineNumber, line)
+			lineNumber++
+			fmt.Printf("%d Updated line: %v\n", lineNumber, updatedLine)
+			lineNumber++
+		}
+	}
+	err=scanner.Err()
+	if err!=nil {
+		fmt.Println("Error scanning file",err)
+		return
+	}
+}
